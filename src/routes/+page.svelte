14a7408663700f7e096e6070904f2924ad9cdfb0
Maco5500 <koeck.maco@johak.at>
2024-03-19 14:54:54 +0100

    hi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,20 @@
 	import { supabase } from '$lib/supabase.js';
 
 	let promise = supabase.from('countries').select();
+	let names = supabase.from('rabbits').select();
+
+	let name = '';
+	async function sendName() {
+		const response = await fetch('/api/hello/sendName', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ name })
+		});
+		const data = await response.text();
+		console.log(data);
+		name = '';
+		names = supabase.from('rabbits').select();
+	}
 </script>
 
 <div class="prose mx-3">
@@ -16,7 +30,32 @@
 		<li>gib die Vercel-URL zu deinem Projekt via Aufgabe im Teams ab</li>
 	</ul>
 	<br />
+	<ul>
+		<li><a href="/api/hello?name=Maco">Link zur API mit Query-Parameter</a></li>
+		<li><a href="/api/hello/name=Maco">Link zur API mit Route-Parameter</a></li>
+	</ul>
+	<br />
 </div>
+
+<div class="prose mx-3">
+	<form>
+		<h1>All my rabbits</h1>
+		<input type="text" bind:value={name} /> <br />
+		<button class="btn" on:click={sendName}>Add Rabbit!</button>
+	</form>
+
+	<h2>Rabbit names:</h2>
+	{#await names}
+		<span class="loading loading-bars loading-lg text-primary" />
+	{:then result}
+		<ul>
+			{#each result.data as rabbit (rabbit.id)}
+				<li>{rabbit.name}</li>
+			{/each}
+		</ul>
+	{/await}
+</div>
+<br />
 
 <div class="prose mx-4">
 	<h2>Liste aller Länder:</h2>
